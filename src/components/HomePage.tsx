13f2ddfcb393ba0f2128feee49dcb6d4b2323776
Izaan <mdizaan67@gmail.com>
2022-08-24 17:09:36 +0530

    fixed authenticate.ts and made landing page responsive

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -5,43 +5,63 @@ import ExploreIcon from '@mui/icons-material/Explore';
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
 import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 const HomePage = () => {
     return (
         <>
-            <nav className="py-4  flex justify-between  items-center relative max-w-[1128px] m-auto ">
-                <div className="">
-                    <Image
-                        width="145"
-                        height="45"
-                        src="/logo-with-text.svg"
-                    ></Image>
+            <nav className="py-2 px-8 sm:py-4 sm:px-16 flex justify-between  items-center relative max-w-[1128px] m-auto">
+                <div className="block min-w-[145px] min-h-[45px]">
+                    <Link href="/">
+                        <Image
+                            src="/logo-with-text.svg"
+                            alt="navbar-logo"
+                            width={145}
+                            height={45}
+                            layout="responsive"
+                            className="min-w-full"
+                        />
+                    </Link>
                 </div>
-                <div className="flex justify-evenly ">
-                    <Link href="/discover">
-                        <div className="mx-4 flex flex-col items-center">
-                            <ExploreIcon className="text-gray-500" />
-                            <p className="font-light text-gray-500">Discover</p>
-                        </div>
-                    </Link>
-                    <Link href="/people">
-                        <div className="mx-4 flex flex-col items-center">
-                            <PeopleAltIcon className="text-gray-500" />
-                            <p className="font-light text-gray-500">People</p>
-                        </div>
-                    </Link>
-                    <Link href="/learning">
-                        <div className="mx-4 flex flex-col items-center">
-                            <OndemandVideoIcon className="text-gray-500" />
-                            <p className="font-light text-gray-500">Learning</p>
-                        </div>
-                    </Link>
-                    <Link href="/jobs">
-                        <div className="mx-4 mr-8 flex flex-col items-center">
-                            <BusinessCenterIcon className="text-gray-500" />
-                            <p className="font-light text-gray-500">Jobs</p>
-                        </div>
-                    </Link>
+                <div className="flex justify-evenly">
+                    <div className="hidden sm:block">
+                        <Link href="/discover">
+                            <div className="mx-4 flex flex-col items-center">
+                                <ExploreIcon className="text-gray-500" />
+                                <p className="font-light text-gray-500">
+                                    Discover
+                                </p>
+                            </div>
+                        </Link>
+                    </div>
+                    <div className="hidden sm:block">
+                        <Link href="/people">
+                            <div className="mx-4 flex flex-col items-center">
+                                <PeopleAltIcon className="text-gray-500" />
+                                <p className="font-light text-gray-500">
+                                    People
+                                </p>
+                            </div>
+                        </Link>
+                    </div>
+                    <div className="hidden sm:block">
+                        <Link href="/learning">
+                            <div className="mx-4 flex flex-col items-center">
+                                <OndemandVideoIcon className="text-gray-500" />
+                                <p className="font-light text-gray-500">
+                                    Learning
+                                </p>
+                            </div>
+                        </Link>
+                    </div>
+                    <div className="hidden sm:block">
+                        <Link href="/jobs">
+                            <div className="mx-4 mr-8 flex flex-col items-center">
+                                <BusinessCenterIcon className="text-gray-500" />
+                                <p className="font-light text-gray-500">Jobs</p>
+                            </div>
+                        </Link>
+                    </div>
 
                     <Link href="/">
                         <a className="px-4 py-2 text-[#0a66c2] rounded-3xl border-[1.5px] border-[#0a66c2] hover:text-[#004182] hover:border-[#004182] hover:bg-[#0a66c21c]">
@@ -51,10 +71,24 @@ const HomePage = () => {
                 </div>
             </nav>
             <section>
-                <div className="flex justify-between min-w-full">
-                    <div className="pl-20 mt-[50px]">
-                        <div className="text-5xl leading-normal text-orange-600 max-w-[45vw]">
+                <div className="sm:flex sm:justify-between">
+                    <div className="mt-8 sm:mt-[50px] sm:max-w-[50vw] w-full">
+                        <div className="p-4 sm:mb-16 sm:pl-24  text-3xl sm:text-5xl leading-normal text-orange-600">
                             <h1>Welcome to your professional community</h1>
+                        </div>
+                        <div className="sm:px-24 mt-4 ">
+                            <GuideButtons
+                                value="Search for a Job"
+                                redirectTo="/jobs"
+                            />
+                            <GuideButtons
+                                value="Find a person you know"
+                                redirectTo="/people"
+                            />
+                            <GuideButtons
+                                value="Learn a new Skill"
+                                redirectTo="/learningS"
+                            />
                         </div>
                     </div>
                     <div className="relative top-0">
@@ -71,3 +105,23 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
+type GuideProps = {
+    value: string;
+    redirectTo: string;
+};
+
+export function GuideButtons(props: GuideProps) {
+    return (
+        <div className="flex justify-between text-gray-700 px-4 py-2 sm:py-4 bg-white sm:rounded my-4">
+            <div className="text-xl ">
+                <h1>{props.value}</h1>
+            </div>
+            <div className="">
+                <Link href={props.redirectTo}>
+                    <ArrowForwardIosIcon />
+                </Link>
+            </div>
+        </div>
+    );
+}
--- src/services/autheticate.ts
+++ src/services/autheticate.ts
@@ -1,18 +1,17 @@
-import { verify } from "jsonwebtoken";
-import { NextApiRequest } from "next";
+import { verify } from 'jsonwebtoken';
+import { NextApiRequest } from 'next';
 
 export const isAuthenticatedFn = (req: NextApiRequest) => {
-    return(
-        verify(
-            req.cookies.auth_token ? req.cookies.auth_token: "authentication",
-            process.env.JWT_SECRET_TOKEN,
-            (err: Error, decode: DecodeSuccessCallback) => {
-                if (!err && decode) {
-                    return true;
-                } else {
-                    return false;
-                }
-            },
-        ),
+    const result = verify(
+        req.cookies.auth_token ? req.cookies.auth_token : 'authentication',
+        process.env.JWT_SECRET_TOKEN,
+        (err: Error, decode: DecodeSuccessCallback) => {
+            if (!err && decode) {
+                return true;
+            } else {
+                return false;
+            }
+        },
     );
+    return result;
 };
