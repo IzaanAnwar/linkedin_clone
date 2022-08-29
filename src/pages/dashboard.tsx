45f8ffddfdd807da7586c64bf0a3e25c7eeb7bac
Izaan <mdizaan67@gmail.com>
2022-08-29 16:14:04 +0530

    added urls for production 2

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -26,7 +26,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
         url:
             process.env.NODE_ENV === 'development'
                 ? `http://localhost:3000/api/posts/`
-                : `https://link-up/api/posts`,
+                : `https://link-up-beta/api/posts`,
         headers: { Cookie: `auth_token=${context.req.cookies.auth_token}` },
     };
     try {
